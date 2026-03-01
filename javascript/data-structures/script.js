0728b70b122c6897919e777d51a68e492dc67200
Kaden Hull <khull0302@gmail.com>
2026-02-28 17:11:23 -0700

    completed data-structures assignment

---
+++ javascript/data-structures/script.js
@@ -0,0 +1,60 @@
+// CHALLENGE 1
+
+// const studentInfo = {
+//   id: 1,
+//   name: {
+//     first: "Kaden",
+//     last: "hull",
+//   },
+//   age: 21,
+//   dob: "03/06/2002",
+//   grades_completed: ["Foundations"],
+//   grades_incompleted: ["backend", "frontend"],
+//   delete_me: 12345,
+// };
+
+// console.log(
+//   `Person ${studentInfo.id}, named ${studentInfo.name.first} ${studentInfo.name.last}, has completed ${studentInfo.grades_completed}.`
+// );
+// console.log(studentInfo);
+// delete studentInfo.delete_me;
+// console.log(studentInfo);
+
+//CHALLENGE 2
+
+// const user = {
+//   firstName: "Kaden",
+//   lastName: "Hull",
+//   city: "Orem",
+//   semesters: {
+//     semesterOne: "incomplete",
+//     semesterTwo: "incomplete",
+//     semesterThree: "incomplete",
+//     semesterFour: "incomplete",
+//   },
+// };
+
+// user.semesters.semesterOne = "complete";
+// console.log(user);
+// delete user.semesters.semesterFour;
+// console.log(user);
+// user["favlanguage"] = "javascript";
+// console.log(user);
+
+// CHALLENGE 3
+// let otherList = ["big", "cat", 2];
+// const myList = [1, "hello", true, null, 3.14, "Kaden"];
+// myList.shift();
+// console.log(myList);
+// myList.pop();
+// console.log(myList);
+// myList.push("skyrim");
+// console.log(myList);
+// myList.unshift(100);
+// console.log(myList);
+
+// const myListCopy = [...myList];
+// console.log(myListCopy);
+
+// newList = [...myList, ...otherList];
+// console.log(newList);
