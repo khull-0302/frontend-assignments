2385507d344ea623a45d7542682d4e4b9fe15422
Kaden Hull <khull0302@gmail.com>
2026-03-24 22:29:28 -0600

    Js capstone complete

--- javascript/js-capstone/scripts/script.js
+++ javascript/js-capstone/scripts/script.js
@@ -1,17 +1,17 @@
 const allNames = document.querySelector(".names-container");
 let students = null;
+const h1 = document.querySelector("h1");
 
 async function fetchUsers() {
   await fetch("https://javascript-capstone-backend.onrender.com/users")
     .then((res) => res.json())
     .then((data) => {
       students = data;
-      console.log(students);
       for (const user of students) {
         div = document.createElement("div");
 
         const p = document.createElement("p");
-        p.innerText = `${user.first_name}: `;
+        p.innerText = `${user.first_name} ${user.last_name[0]}: `;
 
         button1 = document.createElement("button");
         button1.innerText = "+";
@@ -21,33 +21,27 @@ async function fetchUsers() {
         button2.classList.add("minus-button");
 
         user.count = 0;
-        console.log(user);
 
         div.appendChild(p);
         div.appendChild(button1);
         div.appendChild(button2);
         allNames.appendChild(div);
-        console.log(allNames.children);
 
         const span = document.createElement("span");
         span.innerText = user.count;
         p.appendChild(span);
 
         button1.addEventListener("click", () => {
-          user.count += 1;
-          span.innerText = user.count;
-
-          console.log(user.count);
-          console.log(students);
+          if (user.count < 10) {
+            user.count += 1;
+            span.innerText = user.count;
+          }
         });
 
         button2.addEventListener("click", () => {
           if (user.count > 0) {
             user.count -= 1;
             span.innerText = user.count;
-
-            console.log(user.count);
-            console.log(students);
           }
         });
       }
@@ -55,15 +49,46 @@ async function fetchUsers() {
 }
 fetchUsers();
 
-function shuffleUsers() {
+async function shuffleUsers() {
+  // await shuffleAnimation();
+  let weight = [];
   console.log(students);
-  const newArray = students.map((student) => {
-    return student.first_name;
+  for (let student of students) {
+    const studentName = student.first_name + " " + student.last_name[0];
+    console.log(studentName);
+    for (let i = 0; i < student.count; i++) {
+      weight.push(studentName);
+    }
+  }
+  if (weight.length === 0) {
+    return;
+  }
+  await shuffleAnimation();
+  const randomName = weight[Math.floor(Math.random() * weight.length)];
+  h1.innerText = randomName;
+
+  const nameTag = document.querySelectorAll(".names-container div");
+  nameTag.forEach((div) => {
+    if (div.innerText.includes(randomName)) {
+      div.style.color = "red";
+    } else {
+      div.style.color = "";
+    }
   });
-  const randomName = newArray[Math.floor(Math.random() * array.length)];
-  console.log(newArray);
-  console.log(randomName);
 }
 
-// people = ["Kaden", "Eli", "Seth", "Seth", "Jake"];
-shuffleUsers();
+function shuffleAnimation() {
+  return new Promise((resolve) => {
+    const interval = setInterval(() => {
+      const randomIndex = Math.floor(Math.random() * students.length);
+      const student = students[randomIndex];
+
+      h1.innerText = student.first_name + " " + student.last_name[0];
+    }, 200);
+
+    setTimeout(() => {
+      clearInterval(interval);
+      resolve();
+    }, 4000);
+  });
+}
